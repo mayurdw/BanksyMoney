8330d20ffe205d05cdaca92ca1541e69f577e47f
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2023-02-01 08:18:59 +1300

    New Colors Added

--- app/src/main/java/com/mayur/banksymoney/ui/theme/Color.kt
+++ app/src/main/java/com/mayur/banksymoney/ui/theme/Color.kt
@@ -25,3 +25,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val WhiteF4 = Color(0xF4F4F4F4)
+val Black21 = Color(0x21212121)
