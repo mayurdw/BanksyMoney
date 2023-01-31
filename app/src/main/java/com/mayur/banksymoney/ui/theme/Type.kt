c2b778e0e366e270293c55dcd009f7492424e8e5
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2023-02-01 08:19:19 +1300

    New Font added

--- app/src/main/java/com/mayur/banksymoney/ui/theme/Type.kt
+++ app/src/main/java/com/mayur/banksymoney/ui/theme/Type.kt
@@ -18,14 +18,20 @@ package com.mayur.banksymoney.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mayur.banksymoney.R
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
