cbfcdadebd40839d9843dacba3c3eda03c38d560
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2023-02-11 22:18:00 +1300

    GraphicalHistoryView.kt modified and readied

--- app/src/main/java/com/mayur/banksymoney/ui/transactionitem/elements/GraphicalHistoryView.kt
+++ app/src/main/java/com/mayur/banksymoney/ui/transactionitem/elements/GraphicalHistoryView.kt
@@ -2,8 +2,8 @@ package com.mayur.banksymoney.ui.transactionitem.elements
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.jaikeerthick.composable_graphs.color.LinearGraphColors
 import com.jaikeerthick.composable_graphs.composables.LineGraph
@@ -11,24 +11,36 @@ import com.jaikeerthick.composable_graphs.data.GraphData
 import com.jaikeerthick.composable_graphs.style.LineGraphStyle
 
 @Preview(
-    widthDp = 240,
-    heightDp = 320,
+    device = Devices.DEFAULT,
     showBackground = true
 )
 @Composable
-fun GraphicalSummary(
-    modifier: Modifier = Modifier
+private fun PreviewGraphicalSummary() {
+    GraphicalHistoryView(
+        xAxisLabelStrings = listOf(
+            "Sun", "Mon", "Tues"
+        ),
+        yAxisValueIntegers = listOf(
+            200.0, 172.2, 210.2
+        )
+    )
+}
+
+@Composable
+fun GraphicalHistoryView(
+    xAxisLabelStrings: List<String>,
+    yAxisValueIntegers: List<Double>
 ) {
     LineGraph(
-        xAxisData =
-        listOf("Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat").map {
+        xAxisData = xAxisLabelStrings.map {
             GraphData.String(it)
-        }, // xAxisData : List<GraphData>, and GraphData accepts both Number and String types
-        yAxisData = listOf(200, 40, 60, 450, 700, 30, 50),
+        },
+        yAxisData = yAxisValueIntegers,
         style = LineGraphStyle(
             colors = LinearGraphColors(
                 lineColor = MaterialTheme.colorScheme.onBackground,
-                pointColor = MaterialTheme.colorScheme.onBackground,
+                pointColor = MaterialTheme.colorScheme.background,
+                clickHighlightColor = MaterialTheme.colorScheme.onBackground,
                 fillGradient = Brush.verticalGradient(
                     listOf(
                         MaterialTheme.colorScheme.onBackground,
